60d254722eb46fcdacde0c4a2014df6e15204576
ericfoster <eric.foster@sparcedge.com>
2016-07-31 18:14:18 -0400

    create first draft of basic form

--- basicBrainTreeForm.js
+++ basicBrainTreeForm.js
@@ -4,9 +4,15 @@ angular.module('zeatful.angular-braintree-integration', []).
       restrict: 'E',
       replace: true,
       scope: {
-          // scope items
+          amount: "="
       },
-      template: '',
+      template: '<form>' +
+                  '<div class="form-group">'+
+                    '<label for="full-name">Card Holder Full Name</label>'+
+                    '<input type="text" class="form-control" id="full-name" placeholder="Full Name of Card Holder">'+
+                  '</div>'+
+                  '<button type="submit" class="btn btn-default">Submit</button>'+
+                  '</form>'
       link: function(scope, element, attrs) {
           // functional logic
       };
