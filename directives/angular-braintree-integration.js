4ee326db6b6c0f6ed7a9498f6c97042366594b4b
ericfoster <eric.foster@sparcedge.com>
2016-07-31 18:41:16 -0400

    restructure module a bit and attempt to integrate the basic form with the braintree service

---
+++ directives/angular-braintree-integration.js
@@ -0,0 +1,35 @@
+
+var braintreeWeb = require('braintree-web')
+var braintreeFactory = require('./braintree-factory')
+var abi = angular.module('zeatful.angular-braintree-integration', [])
+
+abi.factor('$braintree', [
+  'clientTokenPath',
+  '$http',
+  braintreeFactory(braintreeWeb)
+])
+
+abi.directive('basicBrainTreeForm', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {
+          amount: "=",
+          options: "="
+      },
+      template: '<form id="bt-dropin">' +
+                  '<div class="form-group">'+
+                    '<label for="full-name">Card Holder Full Name</label>'+
+                    '<input type="text" class="form-control" id="full-name" placeholder="Full Name of Card Holder">'+
+                  '</div>'+
+                  '<button type="submit" class="btn btn-default">Submit</button>'+
+                  '</form>',
+      controller: ['$scope', '$braintree', function($scope, $braintree){
+        var options = $scope.options || {}
+        options.container = 'bt-dropin'
+        $braintree.setupDropin(options)
+      }]
+    };
+  });
+
+module.exports = abi;
---
+++ services/braintree-factory.js
@@ -0,0 +1,39 @@
+function braintreeFactory (braintree) {
+  return function braintreeAngular (clientTokenPath, $http) {
+    var $braintree = {}
+
+    $braintree.clientToken = null
+
+    Object.keys(braintree).forEach(function (key) {
+      $braintree[key] = braintree[key]
+    })
+
+    $braintree.getClientToken = function (params) {
+      var path = clientTokenPath
+
+      if (params) {
+        path += '?'
+        path += Object.keys(params).map(function (key) {
+          var value = params[key]
+          return key + '=' + value
+        }).join('&')
+      }
+
+      return $http.get(path)
+    }
+
+    $braintree.setupBasic = function (options) {
+      $braintree.getClientToken()
+        .success(function (token) {
+          braintree.setup(token, 'custom', options)
+        })
+        .error(function (data, status) {
+          console.error('error fetching client token at ' + clientTokenPath, data, status)
+        })
+    }
+
+    return $braintree
+  }
+}
+
+module.exports = braintreeFactory
